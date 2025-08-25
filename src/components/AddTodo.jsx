b0ba982f006c91c7ccb8756df6604e3c3967293b
전수안 <dgsw.ahn@dgsw.hs.kr>
2025-08-25 20:20:28 +0900

    feat: 입력값 없을 시 버튼 비활성화

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -12,12 +12,15 @@ function AddTodo({setHaveTo, haveTo}) {
         setHaveTo([...haveTo, text]);
         inputRef.current.value = '';
         inputRef.current.focus();
+        setText("");
     }
 
     return (
         <div id="Add">
             <input type='text' onChange={onChange} ref={inputRef}/>
-            <button onClick={onClickAdd}>추가</button>
+            <button 
+            onClick={onClickAdd} 
+            disabled={text === "" ? true : false}>추가</button>
         </div>
     )
 }
