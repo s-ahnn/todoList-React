ec7833b0f54a26e867f73e8836d16110b4f74448
전수안 <dgsw.ahn@dgsw.hs.kr>
2025-08-26 21:24:20 +0900

    WIP: 저장 오류

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import AddTodo from './components/AddTodo'
 import Todos from './components/Todos'
 
 function App() {
   const [haveTo, setHaveTo] = useState([]);
+
+  useEffect(() => {
+    try {
+      const savedTodos = localStorage.getItem('savedTodos');
+  
+      if (savedTodos) {
+        const parsedTodo = JSON.parse(savedTodos);
+        setHaveTo(parsedTodo);
+      } else {
+        setHaveTo([]);
+      }
+    } catch (e) {
+      console.error("로컬스토리지 파싱 오류:", e);
+      setHaveTo([]);
+    }
+  }, []);
+  
+  useEffect(() => {
+    try {
+      localStorage.setItem('savedTodos', JSON.stringify(haveTo));
+    } catch (e) {
+      console.error("로컬스토리지 저장 오류:", e);
+    }
+  }, [haveTo]);
 
   return (
     <>
