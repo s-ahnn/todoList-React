e6115c43b94a957e1715a544ed6d64271959891d
전수안 <dgsw.ahn@dgsw.hs.kr>
2025-08-26 19:32:15 +0900

    feat: 체크박스 취소 시 원위치

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,46 +1,51 @@
-function Todos({haveTo, setHaveTo}) {
+function Todos({ haveTo, setHaveTo }) {
     const edit = (id) => {
-        setHaveTo(haveTo.map(item => item.id === id ? {...item, isEdit: !item.isEdit} : item))
+      setHaveTo(haveTo.map(item => item.id === id ? { ...item, isEdit: !item.isEdit } : item))
     }
-
+  
     const handleChangeEdit = (idx, value) => {
-        const newTodos = [...haveTo];
-        newTodos[idx] = value;
-        setHaveTo(newTodos);
+      const newTodos = [...haveTo];
+      newTodos[idx] = value;
+      setHaveTo(newTodos);
     }
-
+  
     const handleClickDelete = (id) => {
-        const newTodos = haveTo.filter((todo) => todo.id !== id);
-        setHaveTo(newTodos)
+      const newTodos = haveTo.filter((todo) => todo.id !== id);
+      setHaveTo(newTodos)
     }
-
+  
     const handleClickCheck = (id) => {
-        setHaveTo(haveTo.map(item => item.id === id ? {...item, isChecked: !item.isChecked} : item))
-        setHaveTo((prev) => {
-            const done = prev.find(todo => todo.id === id);
-            const rest = prev.filter(todo => todo.id !== id);
-            return [...rest, done];
-        })
-    }
-
+      setHaveTo((prev) =>
+        prev.map((item) =>
+          item.id === id ? { ...item, isChecked: !item.isChecked } : item
+        )
+      );
+    };
+  
     return (
-        <div id="Todos">
-            {haveTo.map((item, idx) => {
-                console.log(item, idx);
-                return (
-                    <div className="todoContainer" key={item.id}>
-                        <input type="checkbox" className="check" onClick={() => handleClickCheck(item.id)}/>
-                        <input className={`Todo ${item.isChecked ? "checked" : "nonChecked"}`}
-                        defaultValue={item.text} 
-                        readOnly={item.isEdit === false ? true : false} 
-                        onChange={(e) => handleChangeEdit(item.id, e.target.value)}/>
-                        <button onClick={() => edit(item.id)}>{item.isEdit ? '완료' : '수정'}</button>
-                        <button onClick={() => handleClickDelete(item.id)}>삭제</button>
-                    </div>
-                )
-            })}
-        </div>
+      <div id="Todos">
+        {[...haveTo.filter(todo => !todo.isChecked), ...haveTo.filter(todo => todo.isChecked)]
+          .map((item, idx) => (
+            <div className="todoContainer" key={item.id}>
+              <input
+                type="checkbox"
+                className="check"
+                checked={item.isChecked}
+                onChange={() => handleClickCheck(item.id)}
+              />
+              <input
+                className={`Todo ${item.isChecked ? "checked" : "nonChecked"}`}
+                defaultValue={item.text}
+                readOnly={item.isEdit === false}
+                onChange={(e) => handleChangeEdit(item.id, e.target.value)}
+              />
+              <button onClick={() => edit(item.id)}>{item.isEdit ? '완료' : '수정'}</button>
+              <button onClick={() => handleClickDelete(item.id)}>삭제</button>
+            </div>
+          ))}
+      </div>
     )
-}
-
-export default Todos;
+  }
+  
+  export default Todos;
+  
